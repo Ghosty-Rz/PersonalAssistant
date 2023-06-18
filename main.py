cd4faa85ea704b95eb131f8af6e18f696ec6f8e5
Rihab Zouitni <zouitnirihab@gmail.com>
2023-06-18 21:44:53 +0100

    radical changes

--- main.py
+++ main.py
@@ -1,55 +1,74 @@
-import os
-import time
-import sounddevice as sd
-from gtts import gTTS
 import openai
+import pyttsx3
 import speech_recognition as sr
+import time
 
+# set the api key
 
-api_key = "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"
-#new api key: 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
+openai.api_key = "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"
 
-lang = 'en'
+engine = pyttsx3.init()
 
-openai.api_key = api_key
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+       return recognizer.recognize_google(audio)
+    except:
+        print("skipping unknown error")
 
-person = ""
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt = prompt,
+        max_tokens= 4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
 
-while True:
-    def get_audio():
-        r = sr.Recognizer()
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main ():
+    while True:
+        # wait for the user to say "moon"
+        print("say 'Moon' to start the program")
         with sr.Microphone(device_index=1) as source:
-            audio = r.listen(source)
-            said = ""
-
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
             try:
-                said = r.recognize_google(audio)
-                print(said)
-                global person
-                person = said
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "moon":
+                    # start recording audio
+                    filename = "input.wav"
+                    print("What is your question ...")
+                    with sr.Microphone(device_index=1) as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())
 
-                if "Friday" in said:
+                    # transcribe audio to text
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print (f"you said: {text}")
 
-                    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                              messages=[{"role": "user", "content": said}])
-                    print("do you hear it ")
-                    text = completion.choices[0].messages.content
-                    print(text)
-                    #speech = gTTS(text=text, lang=lang, slow=False, tld="com.au")
-                    #speech.save("welcome1.wav")
+                        # Generate the response using ChatGPT
+                        response = generate_response(text)
+                        print(f"=> {response}")
 
-                    # Play the audio using sounddevice
+                        # read the response
+                        speak_text(response)
 
-
-
-
-            except Exception:
-                print("Exception")
-
-        return said
+            except Exception as e:
+                print("An error has occurred: {}".format(e))
 
 
-    if "stop" in person:
-        break
 
-    get_audio()
+if __name__ == "__main__":
+    main()
